efd5ca9d363a1abd61e26bb5627a094705d179c4
antondukh <antondukh1993@gmail.com>
2024-02-16 23:01:12 +1000

    добавлены функции по чтению json и сортировки по дате

--- functions/funcs.py
+++ functions/funcs.py
@@ -1,6 +1,22 @@
-import json
+def load_json_file():
+    """Чтение файла json и вывод переменной."""
+    import json
+    with open("operations.json", "r", encoding="utf8") as file:
+        operations = file.read()
+        operations_list = json.loads(operations)
+    return operations_list
 
-operations_list = open("operations.json")
-json.load(operations_list)
 
-print(operations_list)
+def sort_by_date():
+    """Сортировка операций по дате и вывод 5-ти последних операций."""
+    sorted_operation_list = []
+    for person in load_json_file():
+        if person:
+            sorted_operation_list.append(person)
+
+    sorted_list = sorted(sorted_operation_list, key=lambda x: x['date'], reverse=True)
+    return sorted_list[0:5]
+
+
+print(sort_by_date())
+
