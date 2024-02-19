121132d4b201752ab783a9416b32d2d01353729a
antondukh <antondukh1993@gmail.com>
2024-02-19 20:37:11 +1000

    исправил вывод from

--- Clases/class_operation.py
+++ Clases/class_operation.py
@@ -2,7 +2,7 @@ import string
 from functions.funcs import last_executed_operation
 
 sort_list = last_executed_operation()
-
+print(sort_list[1])
 
 class Operation:
     def __init__(self, operation):
@@ -17,7 +17,7 @@ class Operation:
     def from_to(self):
         """Вывод откуда -> куда."""
         if 'from' in self.operation:
-            s2 = ''.join(c for c in self.operation['from'] if c in string.ascii_letters)
+            s2 = ''.join([x for x in self.operation['from'] if not x.isdigit()])
             numbers = ''.join(c if c.isdigit() else ' ' for c in self.operation['from']).split()
             from_ = f"{s2} {numbers[0][0:4]} {numbers[0][4:6]}** **** {numbers[0][-4::]}"
             to_ = f"-> {self.operation['to'][0:4]} **{self.operation['to'][-4::]}"
@@ -33,7 +33,7 @@ class Operation:
 
 for i in sort_list:
     gg = Operation(i)
-    print(gg.date())
+    #print(gg.date())
     print(gg.from_to())
-    print(f"""{gg.amount()}
-""")
+    #print(f"""{gg.amount()}
+#""")
