ef06af7800bc1a575751683e2baf3a279685b8c1
antondukh <antondukh1993@gmail.com>
2024-02-21 10:16:15 +1000

    исправил функции и тесты

--- Clases/class_operation.py
+++ Clases/class_operation.py
@@ -2,12 +2,6 @@ class Operation:
     def __init__(self, operation):
         self.operation = operation
 
-    def date(self):
-        """Вывод даты и типа операции."""
-        date_ = f"{self.operation['date'][8:10]}.{self.operation['date'][5:7]}.{self.operation['date'][0:4]}"
-        description_ = self.operation['description']
-        return f"{date_} {description_}"
-
     def from_to(self):
         """Вывод откуда -> куда."""
         if 'from' in self.operation:
@@ -19,7 +13,3 @@ class Operation:
         else:
             to_ = f"{self.operation['to'][0:4]} **{self.operation['to'][-4::]}"
             return to_
-
-    def amount(self):
-        """Вывод суммы операции и валюты."""
-        return f"{self.operation['operationAmount']['amount']} {self.operation['operationAmount']['currency']['name']}"
--- functions/funcs.py
+++ functions/funcs.py
@@ -1,31 +1,42 @@
-def load_json_file():
-    """Чтение файла json."""
-    import json
-    with open("C:/Users/User/PycharmProjects/kursovaya3/functions/operations.json", "r", encoding="utf8") as file:
-        operations = file.read()
-        operations_list = json.loads(operations)
-    return operations_list
+import json
+
+with open("operations.json", 'r', encoding="utf8") as file:
+    operations = file.read()
+    json_file = json.loads(operations)
 
 
-def sort_by_date():
+def sort_by_date(json_file):
     """Сортировка операций по дате."""
     operations_list = []
-    for item in load_json_file():
+    for item in json_file:
         if item:
             operations_list.append(item)
     list_sorted_by_date = sorted(operations_list, key=lambda x: x['date'], reverse=True)
     return list_sorted_by_date
 
 
-def last_executed_operation():
+list_date = sort_by_date(json_file)
+
+
+def last_executed_operation(list_date):
     """Вывод 5-ти последних EXECUTED операций."""
-    sorted_list = []
-    for item in sort_by_date():
+    sorted_lst = []
+    for item in list_date:
         if item['state'] == 'EXECUTED':
-            sorted_list.append(item)
-    return sorted_list[0:5]
+            sorted_lst.append(item)
+            if len(sorted_lst) == 5:
+                break
+    return sorted_lst
 
 
-operations_list = load_json_file()
-list_sorted_by_date = sort_by_date()
-sorted_list = last_executed_operation()
+last_executed_operations = last_executed_operation(list_date)
+
+
+def date_r(last_executed_operations):
+    """Изменение даты в формат dd.mm.yyyy."""
+    for i in last_executed_operations:
+        i['date'] = f"{i['date'][8:10]}.{i['date'][5:7]}.{i['date'][0:4]}"
+    return last_executed_operations
+
+
+right_date = date_r(last_executed_operations)
